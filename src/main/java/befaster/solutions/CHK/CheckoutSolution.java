5da5e3cbe96b76732f76236be251cd49d96c42e8
sbx_user1051 <sbx_user1051@169.254.103.29>
2023-05-19 13:55:32 +0000

    Fri May 19 13:55:32 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,8 +2,24 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckoutSolution {
+
+    static class Offer {
+
+    }
+
+    private final static List<Offer> offers = new ArrayList<>();
+
+    static {
+        Offer offer = new Offer();
+        offers.add(offer);
+    }
+
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
 }
+
