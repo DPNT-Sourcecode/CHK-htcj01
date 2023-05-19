9642269f41362d729a3b7b3e601060ae3d60ce7b
sbx_user1051 <sbx_user1051@169.254.103.29>
2023-05-19 13:58:32 +0000

    Fri May 19 13:58:32 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,8 +7,35 @@ import java.util.List;
 
 public class CheckoutSolution {
 
-    static class Offer {
+    static class OfferCondition {
+        private final String sku;
+        private final Integer quantity;
 
+        public OfferCondition(String sku, Integer quantity) {
+            this.sku = sku;
+            this.quantity = quantity;
+        }
+
+
+
+    }
+
+    static class Offer {
+        private final List<String> expectedSkus;
+        private final Integer finalPrice;
+
+        public Offer(List<String> expectedSkus, Integer finalPrice) {
+            this.expectedSkus = expectedSkus;
+            this.finalPrice = finalPrice;
+        }
+
+        public Integer getFinalPrice() {
+            return finalPrice;
+        }
+
+        boolean isSatisfiedBy(List<String> skuIds){
+            return false;
+        }
     }
 
     private final static List<Offer> offers = new ArrayList<>();
@@ -22,4 +49,5 @@ public class CheckoutSolution {
         throw new SolutionNotImplementedException();
     }
 }
+
 
