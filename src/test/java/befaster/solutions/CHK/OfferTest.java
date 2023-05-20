81955c94b9bc31244d0e7ad70aca165ce3c5b625
sbx_user1051 <sbx_user1051@169.254.145.241>
2023-05-20 07:56:34 +0000

    Sat May 20 07:56:34 UTC 2023

--- src/test/java/befaster/solutions/CHK/OfferTest.java
+++ src/test/java/befaster/solutions/CHK/OfferTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class OfferTest {
 
     @Test
-    void extractBundle() {
+    void extractSimpleBundle() {
         Offer offer = new Offer("STXYZ", new GroupOfferRule(3, "S", "T", "X", "Y", "Z"),  45, null);
 
         Map<String, OrderUnit> unitMap = CheckoutSolution.parseSKUs(List.of("S", "T", "X"));
@@ -18,6 +18,19 @@ class OfferTest {
 
         List<OrderUnit> units = result.getUnits();
         assertEquals(units.size(), 1);
+        assertEquals(units.stream().findFirst().get().getSku(), "STX");
+        assertEquals(units.stream().findFirst().get().getPrice(), 45);
+    }
+
+    @Test
+    void extractMoreComplexBundle() {
+        Offer offer = new Offer("STXYZ", new GroupOfferRule(3, "S", "T", "X", "Y", "Z"),  45, null);
+
+        Map<String, OrderUnit> unitMap = CheckoutSolution.parseSKUs(List.of("S","S", "T", "X"));
+        OfferBundleResult result = offer.extractBundle(unitMap.values().stream().toList());
+
+        List<OrderUnit> units = result.getUnits();
+        assertEquals(units.size(), 2);
         assertEquals(units.stream().findFirst().get().getSku(), "STX");
         assertEquals(units.stream().findFirst().get().getPrice(), 45);
     }
